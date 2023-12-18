2cd1d6b03d243be169849c700fc8ab6c5c81601c
Tsiganov Aleksandr Yevgenevich <Aleksandr.Tsiganov@micros.ucd.uz>
2023-12-18 18:46:59 +0500

    add two pages

---
+++ src/components/Document.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       Страница документа 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts//Layout.vue'),
+    component: () => import('@/layouts/Layout.vue'),
     children: [
       {
-        path: '/users',
+        path: 'users',
         name: 'Users',
         component: () => import('@/components/UsersList.vue'),
       },
       {
-        path: '/docs',
+        path: 'docs',
         name: 'Documents',
         component: () => import('@/components/DocumentsList.vue'),
       },
+      {
+        path: 'users/:id',
+        name: 'User Profile',
+        component: () => import('@/components/UserProfile.vue')
+      },
+      {
+        path: 'docs/:id',
+        name: 'Document',
+        component: () => import('@/components/Document.vue')
+      }
     ],
   },
 ]
